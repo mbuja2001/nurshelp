50691093d1ca1b7feb478cb8e950531b8ef1a133
wtc <wtc@pop-os>
2026-03-25 13:00:25 +0200

    chore: add repo .gitignore to exclude node_modules, venv, large models

--- backend/interaction.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { logInteraction } from "../controllers/interaction.controller.js";
-
-const router = express.Router();
-
-router.post("/log", logInteraction);
-
-export default router;
--- backend/vitals.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { recordVitals } from "../controllers/vitals.controller.js";
-
-const router = express.Router();
-
-router.post("/record", recordVitals);
-
-export default router;
